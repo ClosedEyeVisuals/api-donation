7c0b8f2c9830c0b23f8151c96b1e76cdeb417ac9
Bogdan Komandirov <closedeyevisuals@yandex.ru>
2025-04-19 18:34:42 +0700

    update admin panel

--- donations/admin.py
+++ donations/admin.py
@@ -1,18 +1,26 @@
-from django.db.models import Count
+from django.db.models import Count, Sum
 from django.contrib import admin
 
 
 from donations.models import Collect, Payment
 
 
+class PaymentInline(admin.TabularInline):
+    model = Payment
+    extra = 1
+
+
 @admin.register(Collect)
 class CollectAdmin(admin.ModelAdmin):
+    inlines = (PaymentInline,)
     list_display = (
         'title',
         'reason',
-        'current_value',
         'finish_at',
-        'owner'
+        'owner',
+        'participants',
+        'collected',
+        'goal_value'
     )
     search_fields = (
         'title',
@@ -21,21 +29,27 @@ class CollectAdmin(admin.ModelAdmin):
 
     def get_queryset(self, request):
         queryset = super().get_queryset(request)
-        queryset = queryset.annotate(participants=Count('payments'))
+        queryset = queryset.annotate(participants=Count('payments'),
+                                     collected=Sum('payments__total'))
         return queryset
 
     @admin.decorators.display(description='Участников')
-    def in_favorites_count(self, obj):
+    def participants(self, obj):
         return obj.participants
+
+    @admin.decorators.display(description='Собрано')
+    def collected(self, obj):
+        return obj.collected
 
 
 @admin.register(Payment)
 class PaymentAdmin(admin.ModelAdmin):
     list_display = (
-        'payment_date',
+        'collect',
         'total',
-        'payer',
-        'collect'
+        'payment_date',
+        'payer'
+
     )
     search_fields = (
         'payer',
