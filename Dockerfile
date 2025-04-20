c224ae374b05d591deb4d4f8728f85a3d6898d5b
Bogdan <closedeyevisuals@yandex.ru>
2025-04-20 21:11:36 +0700

    change settings

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ WORKDIR /app
 COPY requirements.txt .
 RUN pip install -r requirements.txt --no-cache-dir
 COPY . .
-CMD ["python", "manage.py", "runserver"]
+CMD ["python", "manage.py", "runserver", "0:8000"]
--- donation_service/settings.py
+++ donation_service/settings.py
@@ -9,7 +9,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = getenv('SECRET_KEY', get_random_secret_key())
-DEBUG = getenv('DEBUG', 'False') == 'True'
+DEBUG = getenv('DEBUG', 'True') == 'True'
 ALLOWED_HOSTS = getenv('ALLOWED_HOSTS', '127.0.0.1').split(', ')
 
 INSTALLED_APPS = [
